aa57bbb414c2a7dd49384993b54f284717b90f9e
billpope <epop.bill@gmail.com>
2021-04-17 12:58:45 -0400

    cont ui work and auth

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
-const functions = require("firebase-functions");
+    const functions = require("firebase-functions");
+    const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    admin.initializeApp();
+
+    const express = require("express");
+    const app = express();
+
+    const authenticate = async function (req, res, next) {
+      if (!req.headers.authorization || !req.headers.authorization.startsWith("Bearer ")) {
+            res.status(403).send('Unauthorized');
+            return;
+        }
+        const idToken = req.headers.authorization.split("Bearer ")[1];
+        try {
+            const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+            req.user = decodedIdToken;
+            next();
+            return;
+        } catch(e) {
+            res.status(403).send('Unauthorized');
+            return;
+        }
+    }
+
+    //app.use(authenticate);
+
+    app.get("/", function (req, res) {
+        res.status(201).json({message:'logged in'});
+    })
+
+    exports.api = functions.https.onRequest(app);
